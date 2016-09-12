db6c3e79c8fdc51a0b9536c7ed0625c9b7175eb9
Michele Andersson <michele@ikkyu.se>
2016-09-13 01:23:45 +0200

    A first try on a simple web api

--- JamesWoods.Api/Controllers/StudentController.cs
+++ JamesWoods.Api/Controllers/StudentController.cs
@@ -3,6 +3,8 @@ using JamesWoods.Service.Dummy;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 
 namespace JamesWoods.Api.Controllers
@@ -26,12 +28,44 @@ namespace JamesWoods.Api.Controllers
         public IHttpActionResult GetStudent(int studentId)
         {
             var oneStudent = allStudents.Where(student => student.Id == studentId).FirstOrDefault();
-            
+
             if (oneStudent == null)
             {
                 return Content(HttpStatusCode.Forbidden, "Studenten finns inte");
             }
             return Ok(oneStudent);
+
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult CreateStudent(Student student)
+        {
+            if (student == null)
+            {
+                return BadRequest("Felaktig inmatning");
+            }
+
+            if (student.FirstName == null)
+            {
+                return BadRequest("Felaktig inmatning, studenter måste ha ett förnamn");
+            }
+
+            //allStudents.Add(student); Eller nåt om du inte hade använt IEnumerable
+            return Content(HttpStatusCode.Created, $"{student.FirstName} är skapad");
+        }
+
+        [Route("{StudentId:int}")]
+        [HttpDelete]
+        public IHttpActionResult DeleteStudent(int studentId)
+        {
+            //var status = allStudents.Where(); Linq?
+            var oneStudent = allStudents.Where(student => student.Id == studentId).FirstOrDefault();
+            if (oneStudent == null)
+            {
+                return Content(HttpStatusCode.Forbidden, "Studenten finns inte");
+            }
+            return Content(HttpStatusCode.OK, "Studenten borttagen!");
         }
     }
 }
